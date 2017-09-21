e60c92331c86eaeb37b88a7c537b522c56001847
kch <kch3338@naver.com>
2017-09-21 10:13:48 +0900

    첫 시작!!

---
+++ src/main/java/com/hanbit/cock/api/CockBackApplication.java
@@ -0,0 +1,12 @@
+package com.hanbit.cock.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CockBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CockBackApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hanbit/cock/api/CockBackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hanbit.cock.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CockBackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
