3a494f437bdd461f61d284240b33c0ab38c82078
kch <kch3338@naver.com>
2017-11-03 17:36:02 +0900

    관리자페이지 맛집/게시글/회원 수 - ch

---
+++ src/main/java/com/hanbit/cock/api/admin/vo/CountVO.java
@@ -0,0 +1,15 @@
+package com.hanbit.cock.api.admin.vo;
+
+public class CountVO {
+	
+	private int count;
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}	
+
+}
