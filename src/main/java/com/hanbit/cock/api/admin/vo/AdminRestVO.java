c3a0518f0b36c7e32b45b425e9b4661c854988e2
kch <kch3338@naver.com>
2017-11-06 12:38:10 +0900

    쿼리문 수정 (cc_rest_detail 추가) - ch

--- src/main/java/com/hanbit/cock/api/admin/vo/AdminRestVO.java
+++ src/main/java/com/hanbit/cock/api/admin/vo/AdminRestVO.java
@@ -4,7 +4,13 @@ public class AdminRestVO {
 	
 	private int rid;
 	private String name;
+	private double lat;
+	private double lng;
 	private int count;
+	private String address;
+	private String phone;
+	private String operating;
+	private String signature;
 	
 	public int getRid() {
 		return rid;
@@ -18,11 +24,47 @@ public class AdminRestVO {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public double getLat() {
+		return lat;
+	}
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+	public double getLng() {
+		return lng;
+	}
+	public void setLng(double lng) {
+		this.lng = lng;
+	}	
 	public int getCount() {
 		return count;
 	}
 	public void setCount(int count) {
 		this.count = count;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getOperating() {
+		return operating;
+	}
+	public void setOperating(String operating) {
+		this.operating = operating;
+	}
+	public String getSignature() {
+		return signature;
+	}
+	public void setSignature(String signature) {
+		this.signature = signature;
 	}	
 
 }
