3c1783c51d9b454c97914a17bdb0c92f65cca0d2
nahcoos <hscsc515@gmail.com>
2017-11-07 10:34:29 +0900

    이미지에 태그가 여러개여도 제대로 나옴, 정렬 쿼리 수정

--- src/main/java/com/hanbit/cock/api/detail/service/DetailService.java
+++ src/main/java/com/hanbit/cock/api/detail/service/DetailService.java
@@ -10,6 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.hanbit.cock.api.detail.dao.DetailDAO;
 import com.hanbit.cock.api.vo.ArticleVO;
 import com.hanbit.cock.api.vo.DetailVO;
+import com.hanbit.cock.api.vo.ImgVO;
+import com.hanbit.cock.api.vo.MenuVO;
 
 @Service
 public class DetailService {
@@ -23,12 +25,11 @@ public class DetailService {
 		if (detail == null) {
 			detail = new DetailVO();
 		}
-		
+
 		// 최신순 / 좋아요순 정렬
 		if (!sort) {
 			detail.setArticles(getArticles(rid));
-		}
-		else {
+		} else {
 			detail.setArticles(getArticlesByLikes(rid));
 		}
 
@@ -44,31 +45,42 @@ public class DetailService {
 		}
 
 		for (int i = 0; i < articles.size(); i++) {
-			articles.get(i).setImgs(detailDAO.selectImgs(articles.get(i)));
+			articles.get(i).setImgs(getImgs(articles.get(i)));
 			articles.get(i).setTags(detailDAO.selectTags(articles.get(i)));
-			articles.get(i).setMenus(detailDAO.selectMenus(articles.get(i)));
 			articles.get(i).setEmblems(detailDAO.selectEmblems(articles.get(i)));
 		}
 
 		return articles;
 	}
-	
+
 	public List<ArticleVO> getArticlesByLikes(int rid) {
 		List<ArticleVO> articles = new ArrayList<>();
 		articles = detailDAO.selectArticlesByLikes(rid);
-		
+
 		if (articles.size() == 0) {
 			detailDAO.deleteRest(rid);
 		}
-		
+
 		for (int i = 0; i < articles.size(); i++) {
-			articles.get(i).setImgs(detailDAO.selectImgs(articles.get(i)));
+			articles.get(i).setImgs(getImgs(articles.get(i)));
 			articles.get(i).setTags(detailDAO.selectTags(articles.get(i)));
-			articles.get(i).setMenus(detailDAO.selectMenus(articles.get(i)));
 			articles.get(i).setEmblems(detailDAO.selectEmblems(articles.get(i)));
 		}
-		
+
 		return articles;
+	}
+
+	public List<ImgVO> getImgs(ArticleVO article) {
+		List<ImgVO> imgs = new ArrayList<>();
+		imgs = detailDAO.selectImgs(article);
+
+		for (int i = 0; i < imgs.size(); i++) {
+			List<MenuVO> menus = new ArrayList<>();
+			menus = detailDAO.selectMenus(article);
+			imgs.get(i).setMenus(menus);
+		}
+
+		return imgs;
 	}
 
 	@Transactional
@@ -77,7 +89,7 @@ public class DetailService {
 		detailDAO.deleteImgs(article);
 		detailDAO.deleteTags(article);
 		detailDAO.deleteArticle(article);
-		
+
 		System.out.println("삭제");
 	}
 
