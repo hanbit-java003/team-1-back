68a3f7651e893bdfe2983ee583cf99433c1878fd
5004 <5004>
2017-11-16 10:53:32 +0900

    update alert article

---
+++ team-1-back/src/main/java/com/hanbit/cock/api/admin/controller/CountController.java
@@ -0,0 +1,36 @@
+package com.hanbit.cock.api.admin.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hanbit.cock.api.admin.service.CountService;
+
+@RestController
+@RequestMapping("/api/cock/admin/count")
+public class CountController {
+	
+	@Autowired
+	private CountService countService;
+	
+	@RequestMapping("/rest")
+	public int adminCountRest() {
+		return countService.adminCountRest();
+	}
+	
+	@RequestMapping("/article")
+	public int adminCountArticle() {
+		return countService.adminCountArticle();
+	}
+	
+	@RequestMapping("/member")
+	public int adminCountMember() {
+		return countService.adminCountMember();
+	}
+	
+	@RequestMapping("/alert/article")
+	public int adminCountAlertArticle() {
+		return countService.adminCountAlertArticle();
+	}
+
+}
